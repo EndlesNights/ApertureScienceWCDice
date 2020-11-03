b2dfd7b23fc963c0bf76d090b7f0a0e35f465348
EndlesNights <58280840+EndlesNights@users.noreply.github.com>
2020-11-03 15:28:29 -0500

    init commit

---
+++ wcube.js
@@ -0,0 +1,52 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({id: "wcube", name: "Aperture Science"}, false);
+	dice3d.addDicePreset({
+		type: "d2",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d2.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d4",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d4.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d6.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d8.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d10.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d100",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d100.glb",
+		system: "wcube"
+	});
+	dice3d.addDicePreset({
+		type: "d12",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d12.glb",
+		system: "wcube"
+	});	
+	dice3d.addDicePreset({
+		type: "d20",
+		labels: "",
+		modelFile: "modules/wcube/wcube_d20.glb",
+		system: "wcube"
+	});
+	
+});
